65263d5216e22546f8849039b2f648d0e520ef4a
AndySouth <southandy@gmail.com>
2016-07-27 22:00:50 +0100

    more examples + trying as a pdf

--- vignettes/tutorial1.R
+++ vignettes/tutorial1.R
@@ -1,12 +1,3 @@
-## ----eval=FALSE, tidy=FALSE----------------------------------------------
-#  #first install devtools to enable install from GitHub
-#  install.packages("devtools")
-#  library(devtools)
-#  install_github('AndySouth/rmapteach')
-
-## ----eval=TRUE-----------------------------------------------------------
-library("rmapteach")
-
 ## ----eval=TRUE-----------------------------------------------------------
 
 # load the package
@@ -25,6 +16,7 @@ ggplot(data = msleep, aes(x = bodywt, y = awake)) +
 
 # aes (for aesthetic) sets up links between the data and aspects of the plot
 # + adds layers to the plot, in this case points
+# geom* stands for the geometric objects you can add to a plot
 # bodywt and awake are columns within the data
 
 # add an extra column to the aes
@@ -35,22 +27,98 @@ ggplot(data = msleep, aes(x = bodywt, y = awake, col = vore)) +
 ggplot(data = msleep, aes(x = bodywt, y = awake, col = sleep_rem)) +
     geom_point()
 
+# you can store the plot as an object (in this case called p) which can make modifying it easier
+p <- ggplot(data = msleep, aes(x = bodywt, y = awake, col = sleep_rem))
+p <- p + geom_point()
+# type the variable name to display the plot 
+p
+
+# use themes to change the overall appearance of plots
+# note that when typing this in RStudio it will show you the options once you've typed th...
+p + theme_bw()
+
 
 ## ----eval=TRUE-----------------------------------------------------------
 
 # we can log bodywt to space the points out more
-ggplot(data = msleep, aes(x = log(bodywt), y = awake, col = vore)) +
-    geom_point()
+p <- ggplot(data = msleep, aes(x = log(bodywt), y = awake, col = vore)) +
+     geom_point()
+p
 
 # label the points for data exploration
-ggplot(data = msleep, aes(x = log(bodywt), y = awake, label = name)) +
-    geom_point() + 
-    geom_text()
+p <- ggplot(data = msleep, aes(x = log(bodywt), y = awake, label = name)) +
+     geom_point() + 
+     geom_text()
+p
 
 # divide into subplots using facet_wrap()
-ggplot(data = msleep, aes(x = log(bodywt), y = awake, label = name)) +
+p <- ggplot(data = msleep, aes(x = log(bodywt), y = awake, label = name)) +
     geom_point() + 
     facet_wrap(~vore)
+p
+
+
+
+
+## ----eval=TRUE-----------------------------------------------------------
+
+#load some time-series data from ggplot2
+data(economics)
+
+# to show the 'str'ucture of the data
+str(economics)
+
+# geom_line()
+ggplot(economics, aes(date, unemploy)) + geom_line()
+
+#note that above aes(date, unemploy) assumes that these are to set x & y respectively
+# this : aes(x=date, y=unemploy) would give an identical result
+
+# another dataset economics_long is in 'long' format
+# instead of having one column for each attribute (e.g. pop and unemploy)
+# it has a column (variable) which has a repeated factor values for each date
+# this allows us to create a plot with multiple lines 
+
+data(economics_long)
+
+ggplot(economics_long, aes(date, value01, colour = variable)) +
+  geom_line()
+
+#note that for each variable the value01 column has been scaled between 0 & 1 to fit on same scale
+
+
+# geom_path can be used to look at the relationship of 2 variables over time
+# unemployment and savings rate
+p <- ggplot(economics, aes(unemploy/pop, psavert))
+p + geom_path(aes(colour = date))
+
+# note how the unemployment rate is calculated by unemploy/pop within the aes call
+# you can do other calculations between columns in there 
+
+
+## ----eval=TRUE-----------------------------------------------------------
+
+# the diamonds dataset has > 50K diamonds (rows) in it
+
+data(diamonds)
+str(diamonds)
+
+# a scatter plot is not very informative
+p <- ggplot(diamonds, aes(x = carat, y = price, col = color))
+p + geom_point()
+
+# setting the alpha (transparency) level to < 1 can improve things but not much
+p <- ggplot(diamonds, aes(x = carat, y = price, col = color))
+p <- p + geom_point(alpha=0.3)
+
+# geom_smooth adds a smoothed conditional mean which can help reveal patterns
+p + geom_smooth()
+
+# geom_histogram can be used to look at distributions 
+p <- ggplot(data=diamonds) + geom_histogram(aes(x=price))
+p
+
+# most of the diamonds are cheap ! (relatively)
 
 
 ## ----eval=FALSE----------------------------------------------------------
@@ -190,29 +258,37 @@ Eu_and_dat <- append_data(Europe, dF, key.shp="name", key.data="country")
 tm_shape(Eu_and_dat) +
   tm_fill("weather", palette=c("red","blue"))
 
-# Can you plot some other data by country ?
-# remember the country names in the map are in Europe$name
-# TODO me to add getting data from another package
 
-
-
-## ----eval=TRUE-----------------------------------------------------------
-# to list datasets in the tmap package
-data(package="tmap")
-
-
-
+## ----eval=FALSE----------------------------------------------------------
+#  
+#  # Can you plot some other data by country ?
+#  # remember the country names in the tmap map are in Europe$name
+#  install.packages("gapminder")
+#  library(gapminder)
+#  ?gapminder
+#  str(gapminder)
+#  
+#  Eu_and_gap <- append_data(Europe, gapminder[gapminder$year==2007,], key.shp="name", key.data="country")
+#  
+#  tm_shape(Eu_and_gap) +
+#     tm_fill("lifeExp")
+#  
 
 ## ----eval=FALSE----------------------------------------------------------
 #  # other useful packages, resources
 #  
-#  # raster package
+#  # packages for creating the circular migration plots I showed this morning
+#  install.packages("migest")
+#  install.packages("circlize")
+#  
+#  
+#  # raster package, great for mapping satellite data etc.
+#  install.packages("raster")
 #  library(raster)
 #  
 #  # rmapshaper for simplifying polygon boundaries
+#  install.packages("rmapshaper")
 #  library(rmapshaper)
-#  
-#  
 #  
 #  
 #  
